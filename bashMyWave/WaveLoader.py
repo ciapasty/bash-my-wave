a04d4e8c84345162f70c7d4dc66c6d6ca930010c
Maciej Eichler <maciej.eichler@gmail.com>
2018-03-31 17:51:56 +0200

    WaveLoader.py: Add loading audio data
      * Currently, all samples are loaded into memory
      * Channels from multichannel wave are stored in list

--- bashMyWave/WaveLoader.py
+++ bashMyWave/WaveLoader.py
@@ -25,40 +25,81 @@ class WaveLoader():
             'chunkDataSize': 0
         }
     }
-
     audioData = []
 
     def loadWavefile(self, filePath):
-        def readFormatData(byteArray):
+        def readASCIItext(binaryFile, nBytes):
+            return bytes(binaryFile.read(nBytes)).decode('ASCII')
+
+        def readInt(binaryFile, nBytes, endian, signed):
+            return int.from_bytes(binaryFile.read(nBytes), endian, signed=signed)
+
+        def readHeader(binaryFile):
+            self.riffHeader['chunkID'] = readASCIItext(binaryFile, 4)
+            self.riffHeader['chunkDataSize'] = readInt(binaryFile, 4, 'little', False)
+            self.riffHeader['riffTypeID'] = readASCIItext(binaryFile, 4)
+            self.riffHeader['fmtHeader']['chunkID'] = readASCIItext(binaryFile, 4)
+            self.riffHeader['fmtHeader']['chunkDataSize'] = readInt(binaryFile, 4, 'little', False)
+            self.riffHeader['fmtData'] = readFormatData(
+                self.riffHeader['fmtHeader']['chunkDataSize'], 
+                binaryFile
+                )
+            # BWF support
+            chunkID = bytes(binaryFile.read(4)).decode('ASCII')
+            chunkDataSize = int.from_bytes(binaryFile.read(4), 'little')
+            while (chunkID != 'data'):
+                self.riffHeader[chunkID+'Header'] = {
+                    'chunkID': chunkID,
+                    'chunkDataSize': chunkDataSize,
+                    # Temporary support for BWF and other Chunks
+                    chunkID+'Data': {}
+                }
+                binaryFile.read(chunkDataSize)
+                chunkID = bytes(binaryFile.read(4)).decode('ASCII')
+                chunkDataSize = int.from_bytes(binaryFile.read(4), 'little')
+
+            self.riffHeader['dataHeader']['chunkID'] = chunkID
+            self.riffHeader['dataHeader']['chunkDataSize'] = chunkDataSize
+        
+        def readFormatData(lenght, binaryFile):
             fmtData = {}
-            fmtData['audioFormat'] = int.from_bytes(byteArray[:2], 'little')
-            fmtData['channels'] = int.from_bytes(byteArray[2:4], 'little')
-            fmtData['samplingRate'] = int.from_bytes(byteArray[4:8], 'little')
-            fmtData['bytesPerSecond'] = int.from_bytes(byteArray[8:12], 'little')
-            fmtData['blockAlign'] = int.from_bytes(byteArray[12:14], 'little')
-            fmtData['bitsPerSample'] = int.from_bytes(byteArray[14:16], 'little')
-            fmtData['extraBytes'] = int.from_bytes(byteArray[16:18], 'little')
+            fmtData['audioFormat'] =    readInt(binaryFile, 2, 'little', False)
+            fmtData['channels'] =       readInt(binaryFile, 2, 'little', False)
+            fmtData['samplingRate'] =   readInt(binaryFile, 4, 'little', False)
+            fmtData['bytesPerSecond'] = readInt(binaryFile, 4, 'little', False)
+            fmtData['blockAlign'] =     readInt(binaryFile, 2, 'little', False)
+            fmtData['bitsPerSample'] =  readInt(binaryFile, 2, 'little', False)
+            fmtData['extraBytes'] =     readInt(binaryFile, 2, 'little', False)
             if (fmtData['extraBytes'] > 0):
-                fmtData['extraFormatInfo'] = readExtraFormatInfo(fmtData['extraBytes'], byteArray[18:])
+                fmtData['extraFormatInfo'] = readExtraFormatInfo(
+                    fmtData['extraBytes'], 
+                    binaryFile.read(fmtData['extraBytes'])
+                    )
 
             return fmtData
 
         def readExtraFormatInfo(length, byteArray):
             return {}
 
-        def readHeader(binaryFile):
-            self.riffHeader['chunkID'] = bytes(binaryFile.read(4)).decode('ASCII')
-            self.riffHeader['chunkDataSize'] = int.from_bytes(binaryFile.read(4), 'little')
-            self.riffHeader['riffTypeID'] = bytes(binaryFile.read(4)).decode('ASCII')
-            self.riffHeader['fmtHeader']['chunkID'] = bytes(binaryFile.read(4)).decode('ASCII')
-            self.riffHeader['fmtHeader']['chunkDataSize'] = int.from_bytes(binaryFile.read(4), 'little')
-            self.riffHeader['fmtData'] = readFormatData(binaryFile.read(self.riffHeader['fmtHeader']['chunkDataSize']))
-            self.riffHeader['dataHeader']['chunkID'] = bytes(binaryFile.read(4)).decode('ASCII')
-            self.riffHeader['dataHeader']['chunkDataSize'] = int.from_bytes(binaryFile.read(4), 'little')
+        def mapChannels(binaryFile, audioChannels, bytesPerChannel):
+            return [readInt(binaryFile, bytesPerChannel, 'little', True) for i in range(audioChannels)]
+
+        def readAudioData(binaryFile):
+            audioChannels = self.riffHeader['fmtData']['channels']
+            audioChunkBytes = self.riffHeader['dataHeader']['chunkDataSize']
+            bytesPerChannel = int(self.riffHeader['fmtData']['blockAlign'] / audioChannels)
+            audioSamples = int(audioChunkBytes/self.riffHeader['fmtData']['blockAlign'])
+
+            if (audioChannels == 1):
+                self.audioData = [readInt(binaryFile, bytesPerChannel, 'little', True) for x in range(audioSamples)]
+            else:
+                self.audioData = [
+                    mapChannels(binaryFile, audioChannels, bytesPerChannel) for x in range(audioSamples)
+                    ]
 
         with open(filePath, 'rb') as binaryFile:
             readHeader(binaryFile)
-
+            readAudioData(binaryFile)
         
 
 #### Debug
@@ -66,7 +107,7 @@ class WaveLoader():
 if __name__ == '__main__':
     wavePath = sys.argv[1]
     
-    wavLoader = WaveLoader()
-    wavLoader.loadWavefile(wavePath)
-    print(wavLoader.riffHeader)
+    waveLoader = WaveLoader()
+    waveLoader.loadWavefile(wavePath)
+    print(waveLoader.riffHeader)
 
